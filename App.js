7f9f22012012365db59c363f19e2fa10a3c57500
@leonardo <leonardobagus093@gmail.com>
2024-01-30 14:07:07 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import RegisterForm from './screens/RegisterForm';
+import LoginForm from './screens/LoginForm';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Register">
+        <Stack.Screen name="Register" component={RegisterForm} />
+        <Stack.Screen name="Login" component={LoginForm} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
